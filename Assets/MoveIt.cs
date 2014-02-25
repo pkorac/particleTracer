a1383666a4253833641a1b15a222b9b6ed27a00c
pkorac <peter.koraca@gmail.com>
2014-02-25 23:10:29 +0000

    initial commit

---
+++ Assets/MoveIt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveIt : MonoBehaviour {
+
+
+	public float perlinGrain = 0.4f;
+	public float scale = 10;
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		
+		float x = Mathf.PerlinNoise( Time.time * perlinGrain, 0.0f ) * scale - scale*0.5f ;
+		float y = 1;
+		float z = Mathf.PerlinNoise( 3000 + Time.time * perlinGrain, 0.0f ) * scale - scale*0.5f;
+		
+
+		Vector3 newPos = new Vector3( x, y, z );
+
+		transform.position = newPos;
+
+	}
+}
---
+++ Assets/Mover2.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover2 : MonoBehaviour {
+
+	public float area = 40;
+
+	private float spacing = 0.1f;
+
+	public int maxRepetitions = 10;
+
+	private int repetition = 0;
+
+	public float speed = 2;
+
+	private Vector3 direction;
+	private Vector3 prevPosition;
+
+	public int step = 0;
+
+
+	private Color gizmoColour;
+
+	void Start () {
+		gizmoColour = Color.green;
+		direction = Vector3.forward;
+		transform.position = new Vector3( -area*0.5f, 1, -area*0.5f);
+		prevPosition = transform.position;
+
+		spacing = 0.5f* area / (float)maxRepetitions;
+		
+	}
+	
+	
+	void Update () {
+		
+		
+		if ( repetition >= maxRepetitions ){
+			ParticleSystem ps = transform.GetComponent<ParticleSystem>();
+			ps.enableEmission = false;
+			gizmoColour = Color.red;
+		} else {
+			if ( step == 0 && Vector3.Distance( prevPosition, transform.position ) >= area ){
+				direction = Vector3.right;
+				prevPosition = transform.position;
+				step = 1;
+				//Debug.Log( "changing to step: " + step );
+			} else if ( step == 1 && Vector3.Distance( prevPosition, transform.position ) >= spacing ){
+				direction = Vector3.back;
+				prevPosition = transform.position;			
+				step = 2;
+				//Debug.Log( "changing to step: " + step );
+			} else if ( step == 2 && Vector3.Distance( prevPosition, transform.position ) >= area ){
+				direction = Vector3.right;
+				prevPosition = transform.position;
+				step = 3;
+				repetition++;
+				//Debug.Log( "changing to step: " + step );
+			} else if ( step == 3 && Vector3.Distance( prevPosition, transform.position ) >= spacing ){
+				direction = Vector3.forward;
+				prevPosition = transform.position;
+				step = 0;
+				//Debug.Log( "changing to step: " + step );				
+			}
+
+			transform.position += direction * speed * Time.deltaTime;
+		}
+
+	
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = gizmoColour;
+		Gizmos.DrawSphere( transform.position, 0.5f );
+	}
+
+}
